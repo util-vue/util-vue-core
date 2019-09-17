7051158cdfa5389694021310eabd406cd12ffe9a
huxingye <huxingye@DESKTOP-OLAEFPL>
2019-09-17 17:09:37 +0800

    1

--- src/lib/store/index.js
+++ src/lib/store/index.js
@@ -1,10 +1,26 @@
 import Vue from "vue";
 import Vuex from "vuex";
 Vue.use(Vuex);
+import article from "./article/article";
+import building from "./building/building";
+import common from "./common/common";
+import customer from "./customer/customer";
+import diyHouseType from "./diy/diyHouseType";
+import diyScheme from "./diy/diyScheme";
+import product from "./product/product";
 import user from "./system/user";
 
 const store = new Vuex.Store({
-  modules: { user },
+  modules: {
+    article,
+    building,
+    common,
+    customer,
+    diyHouseType,
+    diyScheme,
+    product,
+    user
+  },
   strict: process.env.NODE_ENV !== "production"
 });
 
